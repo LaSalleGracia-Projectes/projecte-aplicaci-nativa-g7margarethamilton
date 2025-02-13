5de9a0da91a5d946873c389ed18ffd136cedbf1a
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-02-13 18:41:14 +0100

    add: model de Usuari

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/Model/Usuari.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/Model/Usuari.kt
@@ -1,4 +1,8 @@
 package com.example.projecte_aplicaci_nativa_g7margarethamilton.Model
 
-class Usuari {
-}
+class Usuari(
+    var idUsuari: Int,
+    var nom: String,
+    var email: String,
+    var contrasenya: String
+    )
