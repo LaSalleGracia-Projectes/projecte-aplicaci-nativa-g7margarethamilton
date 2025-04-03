689fb7c429c6f9b721583aed13ac5ebaf7d4a774
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-04-03 18:22:56 +0200

    feat: añadir métodos para gestión de Schedule

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/api/ApiService.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/api/ApiService.kt
@@ -1,11 +1,15 @@
 package com.example.projecte_aplicaci_nativa_g7margarethamilton.api
 import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.Usuari
+import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls.Schedule
 import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.POST
 import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 data class RegisterResponse(
     val message: String,
@@ -26,15 +30,51 @@ data class UserResponse(
 
 interface ApiService {
 
+    //REGISTER
     @POST("auth/register")
     suspend fun register(
         @Body usuario: Usuari
     ): Response<RegisterResponse>
 
+    //LOGIN
     @POST("auth/app/login")
     suspend fun login(
         @Body usuario: Usuari
     ): Response<LoginResponse>
+
+    //GET ALL SCHEDULES
+    @GET("schedule/")
+    suspend fun getAllSchedules(
+        @Body userId: String
+    ): Response<List<Schedule>>
+
+    //GET SCHEDULE BY ID
+    @GET("schedule/{id}")
+    suspend fun getSchedule(
+        @Path("id") id: String,
+        @Body idUsuari: String
+    ): Response<Schedule>
+
+    //CREATE SCHEDULE
+    @POST("schedule/")
+    suspend fun createSchedule(
+        @Body schedule: Schedule
+    ): Response<Any>
+
+    //UPDATE SCHEDULE
+    @POST("schedule/{id}")
+    suspend fun updateSchedule(
+        @Path("id") id: String,
+        @Body schedule: Schedule
+    ): Response<Any>
+
+    //DELETE SCHEDULE
+    @DELETE("schedule/{id}")
+    suspend fun deleteSchedule(
+        @Path("id") id: String,
+        @Body idUsuari: String
+
+    ): Response<Any>
 
 
 
