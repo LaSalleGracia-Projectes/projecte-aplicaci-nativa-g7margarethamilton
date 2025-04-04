119dd40aec31e496fbeed1672b3235bde000eaa2
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-04-04 17:20:35 +0200

    feat: refactor Schedule and Schedule_task classes to use data classes

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/model/moduls/Schedule.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/model/moduls/Schedule.kt
@@ -2,16 +2,17 @@ package com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls
 
 import java.sql.Time
 
-class Schedule(
-    id: Int,
-    title: String,
-    is_favorite: Boolean,
-    email: String,
-    id_category: Int,
-    created_at: Time
+data class Schedule(
+    val id: Int,
+    val title: String,
+    val is_favorite: Boolean,
+    val email: String,
+    val id_category: Int,
+    val created_at: Time,
+    val tasks: List<Schedule_task>
 )
 
-class Schedule_task(
+data class Schedule_task(
     val id: Int,
     val title: String,
     val content: String,
