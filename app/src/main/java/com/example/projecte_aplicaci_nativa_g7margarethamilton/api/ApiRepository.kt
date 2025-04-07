2989dd275763b099cf9a57572fe2e5119b9055a8
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-04-08 01:32:08 +0200

    feat: cambiar parametros para llamadas a la API

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/api/ApiRepository.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/api/ApiRepository.kt
@@ -2,6 +2,7 @@ package com.example.projecte_aplicaci_nativa_g7margarethamilton.api
 
 import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.Usuari
 import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls.Schedule
+import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls.Schedule_task
 
 class ApiRepository {
     val apiInterface = ApiService.create()
@@ -11,9 +12,41 @@ class ApiRepository {
     suspend fun login(usuari: Usuari) = apiInterface.login(usuari)
 
     //SCHEDULE
-    suspend fun getAllSchedules(userId: String) = apiInterface.getAllSchedules(userId)
-    suspend fun getSchedule(id: String, idUsuari: String) = apiInterface.getSchedule(id, idUsuari)
-    suspend fun createSchedule(schedule: Schedule) = apiInterface.createSchedule(schedule)
-    suspend fun updateSchedule(id: String, schedule: Schedule) = apiInterface.updateSchedule(id, schedule)
-    suspend fun deleteSchedule(id: String, idUsuari: String) = apiInterface.deleteSchedule(id, idUsuari)
+    suspend fun getAllSchedules(token: String,) =
+        apiInterface.getAllSchedules("Bearer $token")
+    
+    suspend fun getSchedule(token: String, id: String) =
+        apiInterface.getSchedule("Bearer $token", id)
+    
+    suspend fun createSchedule(token: String, userId: String, title: String, isFavorite: Boolean, categoryId: Int) = 
+        apiInterface.createSchedule(
+            "Bearer $token",
+            CreateScheduleRequest(userId, title, isFavorite, categoryId)
+        )
+    
+    suspend fun updateSchedule(token: String, id: String, userId: String, title: String, isFavorite: Boolean, categoryId: Int) = 
+        apiInterface.updateSchedule(
+            "Bearer $token",
+            id,
+            UpdateScheduleRequest(userId, title, isFavorite, categoryId)
+        )
+    
+    suspend fun deleteSchedule(token: String, id: String, userId: String) = 
+        apiInterface.deleteSchedule("Bearer $token", id)
+
+    //TASK
+    suspend fun createTask(
+        token: String,
+        userId: String,
+        title: String,
+        content: String,
+        priority: Int,
+        startTime: String,
+        endTime: String,
+        scheduleId: Int,
+        categoryId: Int
+    ) = apiInterface.createTask(
+        "Bearer $token",
+        CreateTaskRequest(userId, title, content, priority, startTime, endTime, scheduleId, categoryId)
+    )
 }
