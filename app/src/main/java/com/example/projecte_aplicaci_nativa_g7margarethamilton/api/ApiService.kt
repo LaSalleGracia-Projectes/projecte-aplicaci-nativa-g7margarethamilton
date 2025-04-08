4a8d3c662cb32949b8f185eb1d5565d18bbe8dd4
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-04-08 17:20:11 +0200

    feat: añadir adaptador gson para convertir ZonedDateTime

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/api/ApiService.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/api/ApiService.kt
@@ -1,7 +1,11 @@
 package com.example.projecte_aplicaci_nativa_g7margarethamilton.api
+import ZonedDateTimeAdapter
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.Usuari
 import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls.Schedule
 import com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls.Schedule_task
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -13,6 +17,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.PUT
+import java.time.ZonedDateTime
 
 data class RegisterResponse(
     val message: String,
@@ -91,11 +96,16 @@ interface ApiService {
     companion object{
         private const val BASE_URL = "http://10.0.2.2:3000/api/v1/"
 
+        @RequiresApi(Build.VERSION_CODES.O)
         fun create(): ApiService {
+            val gson = GsonBuilder()
+                .registerTypeAdapter(ZonedDateTime::class.java, ZonedDateTimeAdapter())
+                .create()
+
             val client = OkHttpClient.Builder().build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(client)
                 .build()
             return retrofit.create(ApiService::class.java)
