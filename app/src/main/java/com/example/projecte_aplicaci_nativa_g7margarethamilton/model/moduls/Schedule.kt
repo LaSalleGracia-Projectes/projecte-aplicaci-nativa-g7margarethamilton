85d160304e71734c1620369b15423a2f4854f2cd
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-04-08 17:21:10 +0200

    feat: actualizar created_at como ZonedDateTime y tasks como optional

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/model/moduls/Schedule.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/model/moduls/Schedule.kt
@@ -1,6 +1,7 @@
 package com.example.projecte_aplicaci_nativa_g7margarethamilton.model.moduls
 
 import java.sql.Time
+import java.time.ZonedDateTime
 
 data class Schedule(
     val id: Int,
@@ -8,8 +9,8 @@ data class Schedule(
     val is_favorite: Boolean,
     val email: String,
     val id_category: Int,
-    val created_at: Time,
-    val tasks: List<Schedule_task>
+    val created_at: ZonedDateTime,
+    val tasks: List<Schedule_task>?
 )
 
 data class Schedule_task(
@@ -21,5 +22,5 @@ data class Schedule_task(
     val end_time: String,
     val id_schedule: Int,
     val id_category: Int,
-    val created_at: Time
+    val created_at: ZonedDateTime
 )
