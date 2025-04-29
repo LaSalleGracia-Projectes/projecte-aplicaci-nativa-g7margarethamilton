dce8b857cee28da78998cf60a5cf0ac9f11794a7
JaumeGandara <jaumegandara@gmail.com>
2025-04-29 19:34:51 +0200

    feat: fitxers de i18n

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.1.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
     implementation(libs.accessibility.test.framework)
+    implementation(libs.androidx.appcompat)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
