fbab1ad1574464d17ee3deeb571c63626aabb292
JaumeGandara <jaumegandara@gmail.com>
2025-05-02 00:34:33 +0200

    feat: fitxer i18n

--- app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/viewModel/i18n.kt
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g7margarethamilton/viewModel/i18n.kt
@@ -1,4 +1,12 @@
 package com.example.projecte_aplicaci_nativa_g7margarethamilton.viewModel
 
-class i18n {
+import android.content.Context
+import java.util.Locale
+
+fun Context.setLocale(language: String): Context {
+    val locale = Locale(language)
+    Locale.setDefault(locale)
+    val config = resources.configuration
+    config.setLocale(locale)
+    return createConfigurationContext(config)
 }
